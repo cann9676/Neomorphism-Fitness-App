cc3b649a255ff8bbf9f22cc147c9b78c88d442f7
Chelsea Hannah <80699111+cann9676@users.noreply.github.com>
2023-06-11 21:59:21 -0400

    Initial Commit

---
+++ Neomorphism Fitness App/Neomorphism_Fitness_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Neomorphism_Fitness_AppApp.swift
+//  Neomorphism Fitness App
+//
+//  Created by Chelsea Hannah on 6/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Neomorphism_Fitness_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
